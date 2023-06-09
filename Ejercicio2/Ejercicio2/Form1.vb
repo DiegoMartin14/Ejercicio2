4efc7f7b6e4ce94ac548b390d5dbce2be6b6767f
DiegoMartin14 <diegodanielmartin@hotmail.com>
2023-06-09 12:14:40 -0300

    Commit2

---
+++ Ejercicio2/Ejercicio2/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        Dim b As Integer
+        a = InputBox("Ingresar el primer numero")
+        b = InputBox("Ingresar el segundo numero")
+        If a < b Then
+            Label1.Text = "el segundo numero es mayor y es = " & b
+        Else
+            Label2.Text = "el primer numero es mayor y es = " & a
+        End If
+
+
+    End Sub
+End Class
